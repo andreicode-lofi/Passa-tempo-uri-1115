eaf167b9c8b394c7b5342aabb9973637fc4a9c69
Andrey Souza <andreycode@hotmail.com>
2021-06-29 11:26:25 -0300

    Resolvendo problemas da plataforma uri online

---
+++ Uri1115/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Uri1115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(vet[0]);
+            int y = int.Parse(vet[1]);
+
+            while(x != 0 && y != 0)
+            {
+                if(x > 0 && y > 0)
+                {
+                    Console.WriteLine("Primeiro");
+                }
+                if(x < 0 && y > 0)
+                {
+                    Console.WriteLine("Segundo");
+                }
+                if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("Terceiro");
+                }
+                if(x > 0 && y < 0)
+                {
+                    Console.WriteLine("Quarto");
+                }
+
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+
+            }
+        }
+    }
+}
